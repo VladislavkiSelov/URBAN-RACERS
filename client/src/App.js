9f2a1facf3dd458372c1f75aab5aff1535aa413a
[VladislavkiSelov] <[vladislavkiselev.2007@gmail.com]>
2023-06-24 16:11:39 +0300

    add base str

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './scss/App.scss';
+import MainPage from './pages/MainPage';
+import Header from './section/Header';
+import Footer from './section/Footer';
+
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
